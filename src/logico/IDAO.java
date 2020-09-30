9a8ac546ff91eaf2ed6c60af5397ad5364ef58e6
gustavR0 <gust040825@gmail.com>
2020-09-30 15:03:32 -0500

    integracion GUI

--- src/logico/IDAO.java
+++ src/logico/IDAO.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package logico;
 
 import java.util.List;
 
-/**
- *
- * @author gaddiel
- */
 public interface IDAO<T> {
 
     public boolean insert(T pojo);
