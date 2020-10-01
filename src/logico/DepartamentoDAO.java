8c81600bd6006945faa44fe8cfe567f0813b5d04
alexa <alexa@LAPTOP-IF9JDEPM>
2020-09-30 21:34:26 -0500

    Metodo borrar

--- src/logico/DepartamentoDAO.java
+++ src/logico/DepartamentoDAO.java
@@ -5,7 +5,13 @@
  */
 package logico;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,27 +20,111 @@ import java.util.List;
 public class DepartamentoDAO implements IDAO<Departamento>{
 
     @Override
-    public boolean insert(Departamento pojo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean insert(Departamento pojo)   {
+        
+        Connection con = null;
+        PreparedStatement pst = null;
+        boolean centinela = false;
+        try {
+           
+        
+            String sql = "insert into departamentos(clave, nombre)"+
+                 "values(?, ?)";
+            pst = con.prepareStatement(sql);
+        
+            pst.setString(1, pojo.getClave());
+            pst.setString(2, pojo.getNombre());
+        
+            pst.execute();
+            pst.close();
+        
+        }catch(SQLException ex){
+            
+        }
+        
+        centinela = true;
+        return centinela;
+       
     }
 
     @Override
     public boolean update(Departamento pojo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Connection con = null;
+        PreparedStatement pst = null;
+        boolean centinela = false;
+
+        try {
+
+            String sql = "UPDATE departamentos set clave = ?, nombre = ? "
+                    + "where clave = ? ";
+            pst = con.prepareStatement(sql);
+
+            pst.setString(1, pojo.getClave());
+            pst.setString(2, pojo.getNombre());
+         
+            centinela = pst.execute();
+            pst.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return centinela;
     }
 
     @Override
     public Departamento searchById(Departamento pojo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        PreparedStatement pst = null;
+        String clave, nombre;
+        ResultSet rs;
+        try {
+
+            String sql = "select from departamentos where clave = ?";
+
+            pst = con.prepareStatement(sql);
+            pst.setString(1, pojo.getClave());
+            rs = pst.executeQuery();
+            
+            pst.close();
+            
+            pojo.setClave(rs.getString(1));
+            pojo.setNombre(rs.getString(2));
+            
+            
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return pojo;
+    }
+
+    
+
+    @Override
+    public boolean delete(Departamento pojo) {
+         Connection con = null;
+        PreparedStatement pst = null;
+        boolean centinela = false;
+
+        try {
+
+            String sql = "delete from departamentos where clave = ?";
+            pst = con.prepareStatement(sql);
+
+            pst.setString(1, pojo.getClave());
+
+            centinela = pst.execute();
+
+            pst.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(EmpleadoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return centinela;
     }
 
     @Override
     public List<Departamento> showAll(Departamento pojo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean delete(Departamento pojo) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
